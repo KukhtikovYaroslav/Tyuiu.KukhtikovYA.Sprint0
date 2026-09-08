6be69e210d2d57c1d281d985111041d839411d66
Yaroslav <ykukhtikov@internet.ru>
2026-09-08 16:29:11 +0500

    initial commit

---
+++ Tyuiu.KukhtikovYA.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Tyuiu.KukhtikovYA.Sprint0.Task0.V0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
---
+++ Tyuiu.KukhtikovYA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tyuiu.KukhtikovYA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "Ярослав";
+            int age = 18;
+            
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+            Console.ReadKey();
+        }
+    }
+}
