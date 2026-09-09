bfc63d0790a3384e2202e85bd3d8c7721f65ef0a
Yaroslav <ykukhtikov@internet.ru>
2026-09-09 14:16:36 +0500

    fix MultiplicationArray

--- Tyuiu.KukhtikovYA.Sprint0.Task6.V0/Program.cs
+++ Tyuiu.KukhtikovYA.Sprint0.Task6.V0/Program.cs
@@ -9,7 +9,7 @@ class Program
         int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
         Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
         Console.WriteLine("Разность элементов массива = " + DataService.SubstractionArray(arraynums));
-        Console.WriteLine("Произведение элементов массива = " + DataService.SubstractionArray(arraynums));
+        Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(arraynums));
         Console.ReadKey();
     }
 }
