d6516126882be9212c1adc7f7ca7a62927324e93
Yaroslav <ykukhtikov@internet.ru>
2026-09-09 14:16:01 +0500

    add solutions for task6

---
+++ Tyuiu.KukhtikovYA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KukhtikovYA.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.KukhtikovYA.Sprint0.Task6.V0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
+        Console.WriteLine("Разность элементов массива = " + DataService.SubstractionArray(arraynums));
+        Console.WriteLine("Произведение элементов массива = " + DataService.SubstractionArray(arraynums));
+        Console.ReadKey();
+    }
+}
