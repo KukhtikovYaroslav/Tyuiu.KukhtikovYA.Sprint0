5beb4b2a11b9d7fd74cd305fae0d78b9404ba01c
Yaroslav <ykukhtikov@internet.ru>
2026-09-09 14:01:09 +0500

    add solution for task5

---
+++ Tyuiu.KukhtikovYA.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿namespace Tyuiu.KukhtikovYA.Sprint0.Task5.V0.Lib;
+
+public class DataService
+{
+    public static int Addition(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Subtraction(int a, int b)
+    {
+        return a - b;
+    }
+    
+    public static int Multiplication(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static int Division(int a, int b)
+    {
+        if (b == 0)
+        {
+            Console.WriteLine($"Переменная b = {b} на ноль делить нельзя");
+            return -1;
+        }
+        
+        return a / b;
+    }
+}
